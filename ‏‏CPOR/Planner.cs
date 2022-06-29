226958f04d823c6e1472d221bd64176fe25e59b5
shanigu <shanigu@bgu.ac.il>
2022-06-29 12:03:34 +0300

    Add files via upload

---
+++ ‏‏CPOR/Planner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CPOR
+{
+    abstract class Planner
+    {
+        protected Domain m_dDomain;
+        protected Problem m_pProblem;
+        public Planner(Domain d, Problem p)
+        {
+            m_dDomain = d;
+            m_pProblem = p;
+        }
+
+        public abstract List<Action> Plan(State s);
+    }
+}
---
+++ ‏‏CPOR/RandomGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace CPOR
+{
+    class RandomGenerator
+    {
+        private static Random m_rnd = new Random(0);
+        public static void Init(int iSeed)
+        {
+            Debug.WriteLine("Init random seed to: " + iSeed);
+            m_rnd = new Random(iSeed);
+        }
+        public static void Init()
+        {
+            Debug.WriteLine("Init random seed randomly");
+            m_rnd = new Random();
+        }
+        public static int Next(int iMax)
+        {
+            return m_rnd.Next(iMax);
+        }
+        public static double NextDouble()
+        {
+            return m_rnd.NextDouble();
+        }
+    }
+}
